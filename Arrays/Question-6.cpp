e3a35f52676874abbbae4570a551d24b8c48f5ed
soham Kulkarni <soham18262@gmail.com>
2023-07-05 20:51:14 +0530

    3rd day of challenge

---
+++ Arrays/Question-6.cpp
@@ -0,0 +1,25 @@
+// sorting colors problem
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size()-1;
+        int i = 0;
+        while(i<=high){
+            if(nums[i] == 2){
+                swap(nums[i],nums[high]);
+                high--;
+            }
+            else if(nums[i]==1){
+                i++;
+            }
+            else if(nums[i]==0){
+                swap(nums[i],nums[low]);
+                low++;
+                i++;
+
+            }
+        
+        }
+}
+};
